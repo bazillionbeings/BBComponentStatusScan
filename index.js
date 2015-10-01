f2019447d4d47e6d633c48bf9bfd548770e74858
Ara Yeressian <ayeressian2@gmail.com>
2015-10-01 18:30:43 +0400

    improvements

--- index.js
+++ index.js
@@ -1,72 +1,71 @@
 'use strict';
 
-var http = require('http');
+let http = require('http');
 
-var bbComponent = require('bb-component');
-var constValues = bbComponent.constValues;
-var ComponentResult = bbComponent.ComponentResult;
-var Component = bbComponent.Component;
+let bbComponent = require('bb-component');
+let constValues = bbComponent.constValues;
+let ComponentResult = bbComponent.ComponentResult;
+let Component = bbComponent.Component;
 
-function StatusScan(controllerCallBacks) {
-  Component.call(this, controllerCallBacks, [], []);
+class StatusScan extends Component {
+    constructor(controllerCallBacks) {
+        super(this, controllerCallBacks, [], []);
+        this._FACEBOOK_HOST = 'https://graph.facebook.com';
+    }
+
+    execute() {
+        this._controllerCallBacks.providerRequest(constValues.providerTypes.facebook);
+    }
+
+    _getPosts(graph, onComplete) {
+        graph.get('/me/posts', (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            onComplete(res);
+        });
+    }
+
+    _getNewestMessagePost(postsResponse, onComplete) {
+        let posts = postsResponse.data;
+
+        for (let i = 0; i < posts.length; i++) {
+            if (posts[i].hasOwnProperty('message')) {
+                onComplete(posts[i]);
+                return;
+            }
+        }
+
+        if (postsResponse.paging.next) {
+            let options = {
+                host: this._FACEBOOK_HOST,
+                path: postsResponse.paging.next.replace(this._FACEBOOK_HOST, '')
+            };
+
+            http.request(options, function (response) {
+                let responseStr = '';
+
+                response.on('data', function (chunk) {
+                    responseStr += chunk;
+                });
+
+                response.on('end', function () {
+                    this._getNewestMessagePost(JSON.parse(responseStr), onComplete);
+                });
+            }).end();
+        }
+    }
+
+    setProvider(fbProvider) {
+        this._fbProvider = fbProvider;
+        this._getPosts(fbProvider, response => {
+            this._getNewestMessagePost(response, post => {
+                this._controllerCallBacks.result(new ComponentResult(constValues.componentOutputTypes.facebookLatestStatus, post));
+                this._controllerCallBacks.finish();
+            });
+        });
+    }
 }
-StatusScan.prototype = Object.create(Component.prototype);
-
-StatusScan.prototype.execute = function() {
-  this._controllerCallBacks.providerRequest(constValues.providerTypes.facebook);
-};
-
-StatusScan.prototype._FACEBOOK_HOST = 'https://graph.facebook.com';
-
-StatusScan.prototype._getPosts = function(graph, onComplete) {
-  graph.get('/me/posts', function(err, res) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    onComplete(res);
-  });
-};
-
-StatusScan.prototype._getNewestMessagePost = function(postsResponse, onComplete) {
-  var posts = postsResponse.data;
-
-  for (var i = 0; i < posts.length; i++) {
-    if (posts[i].hasOwnProperty('message')) {
-      onComplete(posts[i]);
-      return;
-    }
-  }
-
-  if (postsResponse.paging.next) {
-    var options = {
-      host: this._FACEBOOK_HOST,
-      path: postsResponse.paging.next.replace(this._FACEBOOK_HOST, '')
-    };
-
-    http.request(options, function(response) {
-      var responseStr = '';
-
-      response.on('data', function(chunk) {
-        responseStr += chunk;
-      });
-
-      response.on('end', function() {
-        this._getNewestMessagePost(JSON.parse(responseStr), onComplete);
-      });
-    }).end();
-  }
-};
-
-StatusScan.prototype.setProvider = function(fbProvider) {
-  var self = this;
-  this._fbProvider = fbProvider;
-  this._getPosts(fbProvider, function(response) {
-    self._getNewestMessagePost(response, function(post) {
-      self._controllerCallBacks.finish(
-        new ComponentResult(constValues.componentOutputTypes.facebookLatestStatus, post));
-    });
-  });
-};
 
 module.exports = StatusScan;
